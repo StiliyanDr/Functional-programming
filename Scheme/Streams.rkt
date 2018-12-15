430e23013936bdd72b41f3a7db6742764495daa3
StiliyanDr <stiliyan.dragnev@gmail.com>
2018-12-15 23:08:15 +0200

    Add new stream functions in Scheme

--- Scheme/Streams.rkt
+++ Scheme/Streams.rkt
@@ -37,12 +37,10 @@
               (take (-- number) (tailOf stream))))))
 
 (define searchStream
-  (lambda (p? stream)
-    (cond
-      [(emptyStream? stream) #f]
-      [(p? (headOf stream)) stream]
-      [else (searchStream p? (tailOf stream))])))
-
+  (lambda (p stream)
+    (and (not (emptyStream? stream))
+         (or (p (headOf stream))
+             (searchStream p (tailOf stream))))))
 
 (define from
   (lambda (number)
@@ -75,5 +73,55 @@
                         (mapSingle function (tailOf stream))))))
     (if (or (emptyStream? stream) (null? streams))
         (mapSingle function stream)
-        (consStream (apply function (cons (headOf stream) (map headOf streams)))
-                    (apply mapStreams (cons function (cons (tailOf stream) (map tailOf streams))))))))
+        (let ([heads (cons (headOf stream) (map headOf streams))]
+              [tails (cons (tailOf stream) (map tailOf streams))])
+          (consStream (apply function heads)
+                      (apply mapStreams (cons function tails)))))))
+
+(define ones (consStream 1 ones))
+
+(define fibs
+  (consStream 0
+              (consStream 1
+                          (mapStreams + fibs (tailOf fibs)))))
+
+(define nats (consStream 0 (mapStreams ++ nats)))
+
+(define divides
+  (lambda (candidate number)
+    (zero? (modulo number candidate))))
+
+(define notDivides
+  (lambda (number)
+    (lambda (candidate)
+      (not (divides number candidate)))))
+
+(define sieve
+  (lambda (stream)
+    (consStream (headOf stream)
+                (sieve (filterStream (notDivides (headOf stream)) (tailOf stream))))))
+
+(define primes (sieve (from 2)))
+
+(define repeat
+  (lambda (item)
+    (consStream item
+                (repeat item))))
+
+(define cycle
+  (lambda (items)
+    (define iterate
+      (lambda (remaining)
+        (if (null? remaining)
+            (iterate items)
+            (consStream (car remaining)
+                        (iterate (cdr remaining))))))
+    (if (null? items)
+        theEmptyStream
+        (iterate items))))
+
+(define iterate
+  (lambda (function x)
+    (consStream x
+                (mapStreams function
+                            (iterate function x)))))
