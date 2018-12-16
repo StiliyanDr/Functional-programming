328c0cbfd27d3382d3b1a3152f6d0b6d1789f2bd
StiliyanDr <stiliyan.dragnev@gmail.com>
2018-12-16 22:45:07 +0200

    Add some of the solutions for the Fourth set of tasks

---
+++ Haskell/Fourth tasks.hs
@@ -0,0 +1,47 @@
+module Fourth tasks where
+import Lists
+import Prelude hiding (all)
+
+sumOfProducts :: Num a => [[a]] -> a
+sumOfProducts lists = sumOf products
+ where products = map productOf lists
+
+occurrences :: Eq item => [item] -> [item] -> [Int]
+occurrences lhs rhs = map (\item -> occurrencesCountOf item rhs) lhs
+
+haveSameLength :: [[item]] -> Bool
+haveSameLength [] = True
+haveSameLength (first : lists) = all hasLengthOfFirst lists
+ where hasLengthOfFirst list = lengthOf list == lengthOfFirst
+       lengthOfFirst = lengthOf first
+
+isSorted :: Ord item => [item] -> Bool
+isSorted [] = True
+isSorted list = all (\(lhs, rhs) -> lhs <= rhs) (zip list (tail list))
+
+isSet :: Ord item => [item] -> Bool
+isSet list = isSorted list && list == uniqueElementsOf list
+
+insertInSortedList :: Ord item => item -> [item] -> [item]
+insertInSortedList item [] = [item]
+insertInSortedList item (head : tail) =
+ if (item < head)
+ then (item : head : tail)
+ else (head : insertInSortedList item tail)
+
+insertInSet :: Ord item => item -> [item] -> [item]
+insertInSet item set = if (isMember item set)
+                       then set
+		       else insertInSortedList item set
+
+union :: Ord item => [item] -> [item] -> [item]
+union lhs rhs = foldRight insertInSet rhs lhs
+
+intersection :: Ord item => [item] -> [item] -> [item]
+intersection lhs rhs = [ item | item <- union lhs rhs,
+                                isMember item lhs,
+				isMember item rhs]
+
+difference :: Eq item => [item] -> [item] -> [item]
+difference lhs rhs = [item | item <- lhs, not (isMember item rhs)]
+
